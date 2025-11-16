7fa8ae90a0cd576121df50f433723c273ea5cdc3
Renato Alves <devrenatoalves@gmail.com>
2025-11-16 20:55:09 -0300

    Leraning about create model with tinker

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Post;
+use App\Models\User;
+
+class PostFactory extends Factory
+{
+    //protected $model = Post::class;
+
+    public function definition(): array
+    {
+        return [
+            'user_id' => function () {
+                return User::factory()->create()->id;
+            },
+            'title' => $this -> faker -> sentence(),
+            'content' => $this -> faker -> text(),
+            'published' => $this -> faker -> boolean(),
+        ];
+    }
+}
